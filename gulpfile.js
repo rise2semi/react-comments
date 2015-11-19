f5851f22ae2e0b777746cfb6b632991aab86fb93
Borys Semerenko <bose@ciklum.com>
2015-11-19 17:45:05 +0200

    Add task to start local server

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var connect = require('gulp-connect');
 
 /**
  * Specify config for tasks
@@ -14,6 +15,22 @@ var config = {
 gulp.task('copy', function () {
     gulp.src( config.index )
         .pipe( gulp.dest( config.dist ) );
+});
+
+/**
+ * Start local server for development
+ */
+gulp.task('server', ['copy'], function () {
+
+    /**
+     * Listening port can be specified manually via command `PORT=7777 gulp`
+     * @type {number}
+     */
+    var serverPort = process.env.PORT || 9000;
+
+    connect.server({
+        port: serverPort
+    });
 });
 
 /**
