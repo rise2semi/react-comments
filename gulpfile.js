5161b4a74002e49c110e1d1c8f2f3a4e11b0b067
Vadim Dyachenko <vadim.dyachenko@bonline.co>
2015-11-30 16:42:53 +0200

    Convert sass to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat-css');
+var sass = require('gulp-sass');
 
 /**
  * Default gulp task
@@ -6,3 +8,11 @@ var gulp = require('gulp');
 gulp.task('default', function () {
    console.log('No task specified');
 });
+/**
+ * Style task
+ */
+ gulp.task('default', function () {
+   gulp.src('src/styles/**/*.sass')
+     .pipe(sass().on('error', sass.logError))
+     .pipe(gulp.dest('dist/css/'));
+ });
