6c084d3d8366f7060a4b49c78614597ef0adc313
Vadim Dyachenko <vadim.dyachenko@bonline.co>
2015-11-30 17:00:23 +0200

    Save as styles.min.css in dist/css folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat-css');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
+var rename = require("gulp-rename");
 
 /**
  * Default gulp task
@@ -13,9 +14,10 @@ gulp.task('default', function () {
  * Style task
  */
  gulp.task('default', function() {
-     gulp.src('src/styles/**/*.sass')
-         .pipe(sass())
-         .pipe(concat('styles.css'))
-         .pipe(minifyCss())
-         .pipe(gulp.dest('dist/css/'));
- });
+    gulp.src('src/styles/**/*.sass')
+        .pipe(sass())
+        .pipe(concat('styles.css'))
+        .pipe(minifyCss())
+        .pipe(rename('styles.min.css'))
+        .pipe(gulp.dest('dist/css/'));
+});
