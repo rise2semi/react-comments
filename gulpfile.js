b281de370ccaf5e8b28439c0e00d89a1ea2cea45
Vadim Dyachenko <vadim.dyachenko@bonline.co>
2015-11-30 16:50:33 +0200

    Add minify plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat-css');
 var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
 
 /**
  * Default gulp task
@@ -11,8 +12,10 @@ gulp.task('default', function () {
 /**
  * Style task
  */
- gulp.task('default', function () {
-   gulp.src('src/styles/**/*.sass')
-     .pipe(sass().on('error', sass.logError))
-     .pipe(gulp.dest('dist/css/'));
+ gulp.task('default', function() {
+     gulp.src('src/styles/**/*.sass')
+         .pipe(sass())
+         .pipe(concat('styles.css'))
+         .pipe(minifyCss())
+         .pipe(gulp.dest('dist/css/'));
  });
